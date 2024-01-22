dffa0f9b2924b41a950e9bf0e8c8d4b3795db47b
Tyler Fenton <tsfento@gmail.com>
2024-01-22 11:31:59 -0600

    Finished Lesson 3

---
+++ lesson_three.sql
@@ -0,0 +1,4 @@
+SELECT * FROM movies WHERE Title LIKE "Toy%";
+SELECT * FROM movies WHERE Director = "John Lasseter";
+SELECT * FROM movies WHERE NOT Director = "John Lasseter";
+SELECT * FROM movies WHERE Title LIKE "WALL%";
