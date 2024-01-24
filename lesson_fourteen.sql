53d8137881de999e8caf2a816eeda141470a4d3d
Tyler Fenton <tsfento@gmail.com>
2024-01-24 10:16:22 -0600

    Finished Lesson 14

---
+++ lesson_fourteen.sql
@@ -0,0 +1,5 @@
+UPDATE Movies SET Director = 'John Lasseter' WHERE Id = 2;
+
+UPDATE Movies SET Year = 1999 WHERE Id = 3;
+
+UPDATE Movies SET Title = 'Toy Story 3', Director = 'Lee Unkrich' WHERE Id = 11;
