3d5dcfb4c0b1bcbf50d1ef41774bae12fce9b15e
Tyler Fenton <tsfento@gmail.com>
2024-01-24 10:11:37 -0600

    Finished Lesson 13

---
+++ lesson_thirteen.sql
@@ -0,0 +1,5 @@
+INSERT INTO Movies (Id, Title, Director, Year, Length_minutes)
+VALUES (4, 'Toy Story 4', 'Josh Cooley', 2019, 100);
+
+INSERT INTO Boxoffice (Movie_id, Rating, Domestic_sales, International_sales)
+VALUES (4, 8.7, 340000000, 270000000);
